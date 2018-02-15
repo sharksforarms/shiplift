08c8af44f8f59b8c29253c6cbb71f7acdd7a729d
Travis CI User <travis@example.org>
2018-02-15 06:17:38 +0000

    Update documentation

---
+++ implementors/rustc_serialize/json/trait.ToJson.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["shiplift"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/rustc-serialize/rustc_serialize/json/trait.ToJson.html\" title=\"trait rustc_serialize::json::ToJson\">ToJson</a> for <a class=\"struct\" href=\"shiplift/builder/struct.ContainerOptions.html\" title=\"struct shiplift::builder::ContainerOptions\">ContainerOptions</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/rustc-serialize/rustc_serialize/json/trait.ToJson.html\" title=\"trait rustc_serialize::json::ToJson\">ToJson</a> for <a class=\"struct\" href=\"shiplift/builder/struct.NetworkCreateOptions.html\" title=\"struct shiplift::builder::NetworkCreateOptions\">NetworkCreateOptions</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/rustc-serialize/rustc_serialize/json/trait.ToJson.html\" title=\"trait rustc_serialize::json::ToJson\">ToJson</a> for <a class=\"struct\" href=\"shiplift/builder/struct.ContainerConnectionOptions.html\" title=\"struct shiplift::builder::ContainerConnectionOptions\">ContainerConnectionOptions</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
